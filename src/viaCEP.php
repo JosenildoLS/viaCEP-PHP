8505f7d1d6c79fe4b39658d5a1ae4e4889b3938b
Bill Barsch <billbarsch@gmail.com>
2020-09-18 16:49:49 -0300

    falta do novo método ddd
    agora existe um novo método que não estava contemplado pela biblioteca

--- src/viaCEP.php
+++ src/viaCEP.php
@@ -2,7 +2,8 @@
 
 namespace JosenildoLS;
 
-class ViaCEP {
+class ViaCEP
+{
 
     const URL_VIACEP = 'http://viacep.com.br/ws/';
     const METHOD = 'json';
@@ -16,8 +17,10 @@ class ViaCEP {
     private $unidade;
     private $ibge;
     private $gia;
+    private $ddd;
 
-    public function find(string $cep = "") {
+    public function find(string $cep = "")
+    {
 
         $this->setCep($cep);
 
@@ -35,26 +38,29 @@ class ViaCEP {
         }
     }
 
-    private function fill(array $attributes) {
+    private function fill(array $attributes)
+    {
 
         foreach ($attributes as $key => $value) {
             $this->{"set" . ucfirst($key)}($value);
         }
-
     }
 
-    public function toJson() {
+    public function toJson()
+    {
 
         $json = json_encode($this->toArray());
         return $json;
     }
 
-    public function toArray(): array {
+    public function toArray(): array
+    {
         $array = get_object_vars($this);
         return $array;
     }
 
-    public function toCSV(string $caminho = "") {
+    public function toCSV(string $caminho = "")
+    {
 
         $data = $this->toArray();
         $headers = array();
@@ -78,7 +84,8 @@ class ViaCEP {
         return $arquivo;
     }
 
-    public function toXML(string $caminho = "") {
+    public function toXML(string $caminho = "")
+    {
         $data = $this->toArray();
         $arquivo = $caminho . DIRECTORY_SEPARATOR . $this->getCep() . ".xml";
 
@@ -104,7 +111,8 @@ class ViaCEP {
         return $arquivo;
     }
 
-    public function toPiped() {
+    public function toPiped()
+    {
         $data = $this->toArray();
         $headers = array();
 
@@ -116,48 +124,69 @@ class ViaCEP {
         return $piped;
     }
 
-    public function toQuerty() {
+    public function toQuerty()
+    {
         $data = $this->toArray();
         return http_build_query($data);
     }
 
-    public function getCep() {
+    public function getDdd()
+    {
+        return $this->ddd;
+    }
+
+    public function getCep()
+    {
         return $this->cep;
     }
 
-    public function getLogradouro() {
+    public function getLogradouro()
+    {
         return $this->logradouro;
     }
 
-    public function getComplement0() {
+    public function getComplement0()
+    {
         return $this->complement;
     }
 
-    public function getBairro() {
+    public function getBairro()
+    {
         return $this->bairro;
     }
 
-    public function getLocalidade() {
+    public function getLocalidade()
+    {
         return $this->localidade;
     }
 
-    public function getUf() {
+    public function getUf()
+    {
         return $this->uf;
     }
 
-    public function getUnidade() {
+    public function getUnidade()
+    {
         return $this->unidade;
     }
 
-    public function getIbge() {
+    public function getIbge()
+    {
         return $this->ibge;
     }
 
-    public function getGia() {
+    public function getGia()
+    {
         return $this->gia;
     }
 
-    private function setCep($cep) {
+    private function setDdd(string $ddd)
+    {
+        $this->ddd = $ddd;
+    }
+
+    private function setCep($cep)
+    {
 
         // retira espacos em branco e mantem apenas numeros
         $cepClean = trim(preg_replace("/[^0-9]/", "", $cep));
@@ -165,36 +194,43 @@ class ViaCEP {
         $this->cep = $cepClean;
     }
 
-    private function setLogradouro(string $logradouro) {
+    private function setLogradouro(string $logradouro)
+    {
         $this->logradouro = $logradouro;
     }
 
-    private function setComplemento(string $complemento) {
+    private function setComplemento(string $complemento)
+    {
         $this->complemento = $complemento;
     }
 
-    private function setBairro(string $bairro) {
+    private function setBairro(string $bairro)
+    {
         $this->bairro = $bairro;
     }
 
-    private function setLocalidade(string $localidade) {
+    private function setLocalidade(string $localidade)
+    {
         $this->localidade = $localidade;
     }
 
-    private function setUf(string $uf) {
+    private function setUf(string $uf)
+    {
         $this->uf = $uf;
     }
 
-    private function setUnidade(string $unidade) {
+    private function setUnidade(string $unidade)
+    {
         $this->unidade = $unidade;
     }
 
-    private function setIbge(string $ibge) {
+    private function setIbge(string $ibge)
+    {
         $this->ibge = $ibge;
     }
 
-    private function setGia(string $gia) {
+    private function setGia(string $gia)
+    {
         $this->gia = $gia;
     }
-
 }
